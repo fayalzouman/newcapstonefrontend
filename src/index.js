1dcb3886f598ef4113a1141e4316bc89b4293310
Fay <fayalzouman@gmail.com>
2019-10-17 17:11:50 +0300

    removed index.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+// import "./in dex.css";
 import App from "./App";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
