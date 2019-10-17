4322e35569408688816a654f369754dbdd6364d5
Fay <fayalzouman@gmail.com>
2019-10-17 14:41:15 +0300

    todays the day

--- src/components/SchoolList/SchoolCard.js
+++ src/components/SchoolList/SchoolCard.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Container } from "react-bootstrap";
 
 function SchoolCard({ school }) {
   return (
-    <div className="col-lg-4 col-md-6 col-12">
-      <Link to={`/categorylist/${school.id}`} className="card">
-        <div className="image">
-          <img className="card-img-top img-fluid" src={school.imageUrl} />
-        </div>
-        <div className="card-body">{school.school_name}</div>
-      </Link>
-    </div>
+    <Container className="col-lg-4 col-md-6 col-12">
+      <div>
+        <Link to={`/categorylist/${school.id}`} className="card">
+          <div className="image">
+            <img className="card-img-top img-fluid" src={school.imageUrl} />
+          </div>
+          <div className="card-body">{school.school_name}</div>
+        </Link>
+      </div>
+    </Container>
   );
 }
 
--- src/components/SubjectList/SubjectDetail.js
+++ src/components/SubjectList/SubjectDetail.js
@@ -21,45 +21,45 @@ class SubjectDetail extends Component {
     const subject = categoriesStore.subject;
     let description = subject.description.split("...");
     let viewedDescription = description.map(des => <div>{des}</div>);
-    return (
-      // <Container style={{ backgroundColor: "white", padding: "20px" }}>
-      <div>
-        <h3 style={{ color: "green", textAlign: "center" }}>
-          {subject.subject_name}
-        </h3>
-        <h5 style={{ color: "red", textAlign: "left" }}>{viewedDescription}</h5>
-        <div style={{ textAlign: "center" }}>
-          <a
-            className="btn btn-success my-2 my-sm-0"
-            href="https://youtube.com/khanacademy"
-            style={{ marginRight: "30px" }}
-          >
-            Learn More
-          </a>
 
-          {/* <img
-            src={author.imageUrl}
-            className="img-thumbnail img-fluid"
-            alt={authorName} />*/}
-          <Link
-            to={`/questionlist/${subject.id}`}
-            className="btn btn-success my-2 my-sm-0"
-          >
-            Take Exam{" "}
-          </Link>
-          <Link
-            to={{
-              pathname: "/createquestion",
-              state: {
-                subjectID: this.props.match.params.subjectdetailID
-              }
-            }}
-          >
-            Add Questions
-          </Link>
-          {/* Link that will take me to the createQuestion page then i will send the subject id in this format subjectID */}
+    return (
+      <Container style={{ backgroundColor: "white", padding: "20px" }}>
+        <div>
+          <h3 style={{ color: "green", textAlign: "center" }}>
+            {subject.subject_name}
+          </h3>
+          <h5 style={{ color: "red", textAlign: "left" }}>
+            {viewedDescription}
+          </h5>
+          <div style={{ textAlign: "center" }}>
+            <a
+              className="btn btn-success my-2 my-sm-0"
+              href="https://youtube.com/khanacademy"
+              style={{ marginRight: "30px" }}
+            >
+              Learn More
+            </a>
+
+            <Link
+              to={`/questionlist/${subject.id}`}
+              className="btn btn-success my-2 my-sm-0"
+            >
+              Take Exam{" "}
+            </Link>
+            <Link
+              to={{
+                pathname: "/createquestion",
+                state: {
+                  subjectID: this.props.match.params.subjectdetailID
+                }
+              }}
+              className="btn btn-success my-2 my-sm-0"
+            >
+              Add Questions
+            </Link>
+          </div>
         </div>
-      </div>
+      </Container>
     );
   }
 }
