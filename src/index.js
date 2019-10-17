f2a2c140ee18cdcfb45590f901b4a9f4b12640f3
Fay <fayalzouman@gmail.com>
2019-10-17 17:07:38 +0300

    removed styling

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-import "./assets/css/nucleo-icons.css";
-import "./assets/scss/blk-design-system-react.scss?v=1.0.0";
-import "./assets/demo/demo.css";
+// import "./assets/css/nucleo-icons.css";
+// import "./assets/scss/blk-design-system-react.scss?v=1.0.0";
+// import "./assets/demo/demo.css";
 
 ReactDOM.render(
   <BrowserRouter>
